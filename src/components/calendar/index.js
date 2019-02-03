8bc21f0a3baeb2c2677564565a5610cc0a7b2693
niklul <nikhil.allamsetti@gmail.com>
2019-02-03 18:05:40 +0530

    successfully getting all the dates from the selected year

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -1,20 +1,31 @@
 import React from 'react'
 import Day from './day'
 
-const NUMBER_OF_DAYS_IN_WEEK = 7
+
+const MAX_YEAR = 2100;
+const MIN_YEAR = 1900;
+const NUMBER_OF_DAYS_IN_WEEK = 7;
 
 
 class Calendar extends React.Component{
 
+    month_options = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    year_options =     Array.from({length: MAX_YEAR - MIN_YEAR}, (v, k) => MIN_YEAR + k+1);
+
+    state={
+        selected_month: 3,
+        selected_year: 2019,
+    }
+
     getDaysCountInMonth = (month, year)=>{
-        return new Date(year, month+1, 0).getDate();
+        return new Date(year, parseInt(month)+1, 0).getDate();
     }
 
     getDatesForMonth = (month, year)=>{
         const dates = [];
-        const end_date = new Date(year, month+1, 0);
-        for (let i = 1; i <=end_date.getDate() ; i++) {
-            dates.push(new Date(year, month+1, i))
+        const days_count = this.getDaysCountInMonth(month, year);
+        for (let i = 1; i <=days_count ; i++) {
+            dates.push(new Date(year, month, i))
         }
         return dates
     }
@@ -30,7 +41,7 @@ class Calendar extends React.Component{
 
     getPostDatesForMonth = (month, year)=>{
         const post_dates = [];
-        const end_date = new Date(year, month+1, 0);
+        const end_date = new Date(year, parseInt(month)+1, 0);
         for (let i = 1; i < NUMBER_OF_DAYS_IN_WEEK-end_date.getDay() ; i++) {
             post_dates.push(new Date(year, month+1, i))
         }
@@ -38,23 +49,63 @@ class Calendar extends React.Component{
     }
 
 
+    handleMonthChange = (e)=>{
+        this.setState({selected_month:e.target.value})
+    }
+
+    handleYearChange = (e)=>{
+        this.setState({selected_year:e.target.value})
+    }
+
+
     render(){
+        const {selected_year, selected_month} = this.state
+
+        const pre_dates = this.getPreDatesForMonth(selected_month, selected_year).map( (date)=>{ return {onClick: '', date: date}});
+        const current_dates = this.getDatesForMonth(selected_month, selected_year).map( (date)=>{ return {onClick: '', date: date}});
+        const post_dates = this.getPostDatesForMonth(selected_month, selected_year).map( (date)=>{ return {onClick: '', date: date}});
+
+        const all_dates = pre_dates.concat(current_dates).concat(post_dates);
+
         return(
             <div>
+                <div>
+                    <span>Select Year:</span>
+                    <span>
+                        <select name="year" onChange={this.handleYearChange} value={selected_year}>
+                            {this.year_options.map((year)=>{
+                                return (
+                                    <option value={year} key={year} >{year}</option>
+                                )
+                            })
+                            }
+                        </select>
+                    </span>
+                </div>
 
-                {this.getPreDatesForMonth(4, 2019).map(
-                    (date)=>{
-                        return (<Day date={date}/>)
-                    })}
-                {this.getDatesForMonth(4, 2019).map(
-                    (date)=>{
-                        return (<Day date={date}/>)
-                    })}
+                <div>
+                    <span>Select Month:</span>
+                    <span>
+                        <select name="month" onChange={this.handleMonthChange} value={selected_month}>
+                            {this.month_options.map((month, index)=>{
+                                return (
+                                    <option value={index} key={index} >{month}</option>
+                                )
+                            })
 
-                {this.getPostDatesForMonth(4, 2019).map(
-                    (date)=>{
-                        return (<Day date={date}/>)
-                    })}
+                            }
+
+                        </select>
+                    </span>
+                </div>
+
+                <div>
+                    <table>
+                        <tr>
+                            <td></td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         )
     }
